8c95374747c300aecf77bdff3283b8116554a74e
zTig3r <timo1bauer@gmx.de>
2025-08-24 14:02:50 +0200

    add .gitignore, update gradle wrapper and scripts to version 9.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.0.2"
 }
 
 group = "de.ztiger"
@@ -12,17 +12,17 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.twitch4j:twitch4j:1.21.0")
+    implementation("com.github.twitch4j:twitch4j:1.25.0")
     implementation("net.dv8tion:JDA:5.3.0") {
         exclude(module="opus-java")
     }
-    implementation ("ch.qos.logback:logback-classic:1.5.10")
+    implementation ("ch.qos.logback:logback-classic:1.5.13")
     implementation ("org.mariadb.jdbc:mariadb-java-client:3.4.1")
     implementation ("org.slf4j:slf4j-api:2.0.12")
     implementation ("io.github.cdimascio:dotenv-java:3.0.0")
     implementation ("com.vaadin.external.google:android-json:0.0.20131108.vaadin1")
     implementation ("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8.4")
-    implementation ("org.apache.httpcomponents:httpclient:4.5.13")
+    implementation ("org.apache.httpcomponents:httpclient:4.5.14")
 }
 
 tasks.withType(JavaCompile::class.java) {
